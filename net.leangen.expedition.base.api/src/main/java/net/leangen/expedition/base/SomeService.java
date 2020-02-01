e619c272d0ec1738cd90b26d0565933a54d88754
David Leangen <david@leangen.net>
2020-02-01 13:08:46 +0900

    Initial commit

---
+++ net.leangen.expedition.base.api/src/main/java/net/leangen/expedition/base/SomeService.java
@@ -0,0 +1,6 @@
+package net.leangen.expedition.base;
+
+public interface SomeService
+{
+
+}
---
+++ net.leangen.expedition.base.api/src/test/java/net/leangen/expedition/base/BasicJUnitTest.java
@@ -0,0 +1,15 @@
+package net.leangen.expedition.base;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasicJUnitTest
+{
+	@Test
+	public void shouldCombineItemsOfSameType()
+	{
+        assertTrue(true);
+	}
+
+}
